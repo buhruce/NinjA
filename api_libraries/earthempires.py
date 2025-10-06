069947703fce735dd48f752ffe9467a0624559a1
buhruce <43326359+buhruce@users.noreply.github.com>
2025-10-06 07:22:10 -0700

    New round and dates.

--- api_libraries/earthempires.py
+++ api_libraries/earthempires.py
@@ -66,7 +66,7 @@ def player_land(df):
 
 def coop_info():
     server_id = "22"
-    round = "8"
-    start = "2025-08-05 00:00"
-    end = "2025-10-03 23:59"
+    round = "9"
+    start = "2025-010-05 00:00"
+    end = "2025-12-03 23:59"
     return server_id, round, start, end
